c857076e48ba8eb4d5d659dda39120b8e062eae8
nor304 <phanbac3042000@gmail.com>
2020-12-25 18:21:41 +0700

    Update favorite controller and router

--- server/routes/favorite.route.js
+++ server/routes/favorite.route.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const favoriteController = require('../controllers/favorite.controller');
 
-router.get('/:user_id', favoriteController.getByUser);
-
-router.delete('/user/:place_id', favoriteController.delete);
+router.get('/:user_id', favoriteController.getById);
+router.post('/:user_id', favoriteController.add);
+router.delete('/:place_id', favoriteController.delete);
 
 module.exports = router;
